ccbd71eeb1e88ad7b1a6e3e7a34246f00db7fea4
Ornelo <Ornelo@Ornelo-PC.fritz.box>
2015-07-29 17:25:01 +0200

    test 5

--- AntTest01/src/com/ant/test/HelloAnt.java
+++ AntTest01/src/com/ant/test/HelloAnt.java
@@ -4,7 +4,7 @@ public class HelloAnt {
 
 	public static void main(String[] args) {
 	
-		System.out.println("First ANT code in Eclipse ... TEST With GIT");
+		System.out.println("First ANT code in Eclipse ... TEST  5 With GIT");
 
 	}
 
